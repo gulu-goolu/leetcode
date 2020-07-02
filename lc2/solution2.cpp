335a0aeabd07472fce868d072464543a24a09c1a
murmur wheel <murmur.wheel@gmail.com>
2020-07-03 01:08:37 +0800

    update readme.md

--- lc2/solution2.cpp
+++ lc2/solution2.cpp
@@ -2,3 +2,18 @@
 // Created by chj on 2020/7/3.
 //
 
+// https://leetcode-cn.com/problems/add-two-numbers/
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  explicit ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// 地归
+class Solution {
+ public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) { return nullptr; }
+};
+
+int main() {}
