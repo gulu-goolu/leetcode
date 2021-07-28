5d9d6fbfd575f2dd85831a72ad92c0704b3c332a
caohongjian <caohongjian@bigo.sg>
2021-07-28 12:23:14 +0800

    add develop environment

---
+++ cmake/module/FindGoogleTest.cmake
@@ -0,0 +1,6 @@
+include (FetchContent)
+
+FetchContent_Declare(googletest
+    URL https://github.com/google/googletest/archive/refs/tags/release-1.11.0.tar.gz
+)
+FetchContent_MakeAvailable(googletest)
